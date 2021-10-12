cc2f3e1a36a6351bd128e7b045b6d9ffa07147cb
hermanlyx <hermanlyx@gmail.com>
2021-10-12 10:44:00 +0800

    added next and previous page functionality

--- src/components/pages/Exercises.jsx
+++ src/components/pages/Exercises.jsx
@@ -33,16 +33,27 @@ function Exercises() {
     }
   };
 
-  const fetchExercises = async () => {
-    try {
-      const res = await fetch(
-        `https://wger.de/api/v2/exercise/?format=json&language=2${checkedCategories}${checkedEquipment}`
-      );
-      const data = await res.json();
-      console.log("json data", data);
-      setExercises(data);
-    } catch (e) {
-      console.log("SOMETHING WENT WRONG!!!", e);
+  const fetchExercises = async (link) => {
+    if (link) {
+      try {
+        const res = await fetch(link);
+        const data = await res.json();
+        //   console.log("json data", data);
+        setExercises(data);
+      } catch (e) {
+        console.log("SOMETHING WENT WRONG!!!", e);
+      }
+    } else {
+      try {
+        const res = await fetch(
+          `https://wger.de/api/v2/exercise/?format=json&language=2${checkedCategories}${checkedEquipment}`
+        );
+        const data = await res.json();
+        //   console.log("json data", data);
+        setExercises(data);
+      } catch (e) {
+        console.log("SOMETHING WENT WRONG!!!", e);
+      }
     }
   };
 
@@ -95,7 +106,7 @@ function Exercises() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("checked Items", checkedItems);
+    // console.log("checked Items", checkedItems);
     const categoryValues = [];
     const equipmentValues = [];
     for (let i = 0; i < checkedItems.length; i++) {
@@ -107,14 +118,14 @@ function Exercises() {
     }
     setCheckedEquipment("&equipment=" + equipmentValues.toString());
     setCheckedCategories("&category=" + categoryValues.toString());
-    console.log("checked eq", checkedEquipment);
-    console.log("checked cat", checkedCategories);
+    // console.log("checked eq", checkedEquipment);
+    // console.log("checked cat", checkedCategories);
   };
 
   const handleChange = (event) => {
-    console.log(event.target.type);
+    // console.log(event.target.type);
     if (event.target.type === "checkbox") {
-      console.log("checked?", event.target.checked);
+      //   console.log("checked?", event.target.checked);
       event.target.checked
         ? setCheckedItems([
             ...checkedItems,
@@ -145,6 +156,47 @@ function Exercises() {
     }
   };
 
+  const createNextPreviousPageButtons = () => {
+    console.log("exercises", exercises);
+    return (
+      <div className="flex-1 flex justify-center">
+        {exercises?.previous ? createPreviousPage(exercises.previous) : null}
+        {exercises?.next
+          ? createNextPage(exercises.next)
+          : showNoMoreExercises()}
+      </div>
+    );
+  };
+
+  const createPreviousPage = () => {
+    return (
+      <button
+        onClick={() => fetchExercises(exercises.previous)}
+        className="w-40 m-2 relative px-4 py-2 border border-blue text-sm text-center font-medium rounded-md text-blue bg-white hover:bg-darkblue hover:text-white"
+      >
+        Previous page
+      </button>
+    );
+  };
+
+  const createNextPage = () => {
+    return (
+      <button
+        onClick={() => fetchExercises(exercises.next)}
+        className="w-40 m-2 relative px-4 py-2 border border-blue text-sm text-center font-medium rounded-md text-blue bg-white hover:bg-darkblue hover:text-white"
+      >
+        Next page
+      </button>
+    );
+  };
+
+  const showNoMoreExercises = () => {
+    return (
+      <p className="w-40 m-2 relative px-4 py-2 text-sm text-center font-medium rounded-md text-white bg-blue">
+        No More Exercises
+      </p>
+    );
+  };
   //   console.log("options", filters[0].options, filters[1].options);
 
   return (
@@ -226,7 +278,7 @@ function Exercises() {
                 </div>
               </div>
             </div>
-            <button className="font-bold text-white bg-blue p-4 rounded-lg my-4">
+            <button className="font-bold text-white bg-blue p-4 rounded-lg my-4 hover:bg-darkblue">
               Search Exercises
             </button>
           </div>
@@ -237,33 +289,14 @@ function Exercises() {
       </div>
 
       <div className="bg-white px-4 py-3 flex items-center justify-center sm:px-6">
-        <div className="flex-1 flex justify-center">
+        {createNextPreviousPageButtons()}
+        {/* <div className="flex-1 flex justify-center">
           <button className="w-40 m-2 relative px-4 py-2 border border-blue text-sm text-center font-medium rounded-md text-blue bg-white hover:bg-darkblue hover:text-white">
             Previous page
           </button>
-          <button
-            href="#"
-            className="w-40 m-2 relative px-4 py-2 border border-blue text-sm text-center font-medium rounded-md text-blue bg-white hover:bg-darkblue hover:text-white"
-          >
+          <button className="w-40 m-2 relative px-4 py-2 border border-blue text-sm text-center font-medium rounded-md text-blue bg-white hover:bg-darkblue hover:text-white">
             Next page
           </button>
-        </div>
-        {/* <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-center">
-          <div>
-            <nav
-              className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
-              aria-label="Pagination"
-            >
-              <button className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-blue bg-white text-sm font-medium text-blue hover:bg-darkblue hover:text-white">
-                <span className="sr-only">Previous</span>
-                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
-              <button className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-blue bg-white text-sm font-medium text-blue hover:bg-darkblue hover:text-white">
-                <span className="sr-only">Next</span>
-                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
-            </nav>
-          </div>
         </div> */}
       </div>
     </>
