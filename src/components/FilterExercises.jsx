4429633867d408b32edc16fe673a9dc23f8c3141
hermanlyx <hermanlyx@gmail.com>
2021-10-13 10:34:15 +0800

    moved filter into its own component

---
+++ src/components/FilterExercises.jsx
@@ -0,0 +1,105 @@
+function FilterExercises(props) {
+  const filters = [
+    {
+      id: "equipment",
+      name: "Equipment",
+      options: props.equipment.map((item) => {
+        return {
+          equipmentId: item.id,
+          equipmentName: item.name,
+          type: "equipment",
+        };
+      }),
+    },
+    {
+      id: "category",
+      name: "Category",
+      options: props.categories.map((item) => {
+        return {
+          categoryId: item.id,
+          categoryName: item.name,
+          type: "category",
+        };
+      }),
+    },
+  ];
+
+  return (
+    <div className="max-w-2xl mx-auto pt-7 px-4 sm:pt-10 sm:px-6 lg:max-w-7xl lg:px-8">
+      <form onSubmit={props.handleSubmit}>
+        <div className="grid grid-cols-1 gap-1">
+          <h1 className="text-xl font-bold text-blue w-full sm:text-2xl pb-2">
+            Filter Exercises
+          </h1>
+          <div className="border-blue border-solid border-2 rounded-md pb-5">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-1">
+              <div>
+                <h3 className="text-lg font-bold text-blue p-5">Equipment</h3>
+                <div className="grid grid-cols-2 flex-wrap px-5">
+                  {filters[0]?.options?.map((item, index) => {
+                    // console.log("equipments", item);
+                    return (
+                      <div
+                        key={item.equipmentName}
+                        className="flex items-center"
+                      >
+                        <input
+                          type="checkbox"
+                          name={item.type}
+                          id={index}
+                          value={item.equipmentId}
+                          onChange={props.handleChange}
+                          className="text-blue"
+                        />
+                        <label
+                          htmlFor={item.equipmentName}
+                          className="text-md font-medium text-blue py-1 px-3"
+                        >
+                          {item.equipmentName}
+                        </label>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div>
+                <h3 className="text-lg font-bold text-blue p-5">Target Area</h3>
+                <div className="grid grid-cols-2 flex-wrap px-5">
+                  {filters[1]?.options?.map((item, index) => {
+                    // console.log("category", item);
+                    return (
+                      <div
+                        key={item.categoryName}
+                        className="flex items-center"
+                      >
+                        <input
+                          type="radio"
+                          name={item.type}
+                          id={index}
+                          value={item.categoryId}
+                          onChange={props.handleChange}
+                          className="text-blue"
+                        />
+                        <label
+                          htmlFor={item.categoryName}
+                          className="text-md font-medium text-blue py-1 px-3"
+                        >
+                          {item.categoryName}
+                        </label>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+          <button className="font-bold text-white bg-blue p-4 rounded-lg my-4 hover:bg-darkblue">
+            Search Exercises
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default FilterExercises;
--- src/components/pages/Exercises.jsx
+++ src/components/pages/Exercises.jsx
@@ -2,6 +2,7 @@ import { Fragment, useRef, useState, useEffect } from "react";
 import ShowExercises from "../ShowExercises";
 import NextPreviousPageButtons from "../NextPreviousPageButtons";
 import { Dialog, Transition } from "@headlessui/react";
+import FilterExercises from "../FilterExercises";
 
 function Exercises(props) {
   const [categories, setCategories] = useState([]);
@@ -70,31 +71,6 @@ function Exercises(props) {
     fetchExercises();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [checkedCategories, checkedEquipment]);
-
-  const filters = [
-    {
-      id: "equipment",
-      name: "Equipment",
-      options: equipment.map((item) => {
-        return {
-          equipmentId: item.id,
-          equipmentName: item.name,
-          type: "equipment",
-        };
-      }),
-    },
-    {
-      id: "category",
-      name: "Category",
-      options: categories.map((item) => {
-        return {
-          categoryId: item.id,
-          categoryName: item.name,
-          type: "category",
-        };
-      }),
-    },
-  ];
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -169,82 +145,12 @@ function Exercises(props) {
           </h1>
         </div>
       </div>
-      <div className="max-w-2xl mx-auto pt-7 px-4 sm:pt-10 sm:px-6 lg:max-w-7xl lg:px-8">
-        <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-1 gap-1">
-            <h1 className="text-xl font-bold text-blue w-full sm:text-2xl pb-2">
-              Filter Exercises
-            </h1>
-            <div className="border-blue border-solid border-2 rounded-md pb-5">
-              <div className="grid grid-cols-1 lg:grid-cols-2 gap-1">
-                <div>
-                  <h3 className="text-lg font-bold text-blue p-5">Equipment</h3>
-                  <div className="grid grid-cols-2 flex-wrap px-5">
-                    {filters[0]?.options?.map((item, index) => {
-                      // console.log("equipments", item);
-                      return (
-                        <div
-                          key={item.equipmentName}
-                          className="flex items-center"
-                        >
-                          <input
-                            type="checkbox"
-                            name={item.type}
-                            id={index}
-                            value={item.equipmentId}
-                            onChange={handleChange}
-                            className="text-blue"
-                          />
-                          <label
-                            htmlFor={item.equipmentName}
-                            className="text-md font-medium text-blue py-1 px-3"
-                          >
-                            {item.equipmentName}
-                          </label>
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-                <div>
-                  <h3 className="text-lg font-bold text-blue p-5">
-                    Target Area
-                  </h3>
-                  <div className="grid grid-cols-2 flex-wrap px-5">
-                    {filters[1]?.options?.map((item, index) => {
-                      // console.log("category", item);
-                      return (
-                        <div
-                          key={item.categoryName}
-                          className="flex items-center"
-                        >
-                          <input
-                            type="radio"
-                            name={item.type}
-                            id={index}
-                            value={item.categoryId}
-                            onChange={handleChange}
-                            className="text-blue"
-                          />
-                          <label
-                            htmlFor={item.categoryName}
-                            className="text-md font-medium text-blue py-1 px-3"
-                          >
-                            {item.categoryName}
-                          </label>
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              </div>
-            </div>
-            <button className="font-bold text-white bg-blue p-4 rounded-lg my-4 hover:bg-darkblue">
-              Search Exercises
-            </button>
-          </div>
-        </form>
-      </div>
+      <FilterExercises
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        equipment={equipment}
+        categories={categories}
+      />
       <div className="max-w-2xl mx-auto pt-3 px-4 sm:pt-5 sm:px-6 lg:max-w-7xl lg:px-8">
         <ShowExercises
           exercises={exercises}
